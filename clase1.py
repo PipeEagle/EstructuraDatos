6ab912f7f14b432dd3f7918136bc1ccfa5923ac0
PipeEagle <134529744+PipeEagle@users.noreply.github.com>
2026-02-07 16:35:06 +0000

    codigo de la primera clase

---
+++ clase1.py
@@ -0,0 +1,10 @@
+edad = int(input("Ingrese su edad: "))
+print("Tu edad es:", edad >= 18)
+
+nombre = input("Ingrese su nombre: ")
+print("Tu nombre es:", nombre)
+
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:    
+    print("Eres menor de edad.")              
