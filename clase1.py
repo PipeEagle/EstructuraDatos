111e049b7ccc3d42ec143d05c270dd9454c5b3dd
PipeEagle <pipegomezvelez@gmail.com>
2026-02-13 19:14:38 -0500

    Add functionality to evaluate student grades and implement loops in practica.py; update clase1.py to include favorite class input

--- clase1.py
+++ clase1.py
@@ -4,7 +4,16 @@ print("Tu edad es:", edad >= 18)
 nombre = input("Ingrese su nombre: ")
 print("Tu nombre es:", nombre)
 
+clase = input("Ingrese su clase favorita: ")
+print("Tu clase favorita es:", clase)
+
 if edad >= 18:
     print("Eres mayor de edad.")
 else:    
     print("Eres menor de edad.")               
+
+
+
+
+
+
---
+++ practica.py
@@ -0,0 +1,39 @@
+nota = float (input("Ingrese la nota del estudiante: "))
+
+# Evaluar la nota y mostrar el resultado con condicionales (IF, ELIF, ELSE)
+
+if nota < 5:
+    print("El estudiante ha reprobado.")
+elif nota >= 5 and nota < 7:
+    print("El estudiante ha aprobado con una nota regular.")
+elif nota >= 7 and nota < 9:
+    print("El estudiante ha aprobado con una nota buena.")
+elif nota >= 9 and nota <= 10:
+    print("El estudiante ha aprobado con una nota excelente.")
+else:
+    print("La nota ingresada no es válida.") 
+    
+#Bucles con WHILE
+
+contador = 10
+
+while contador > 0:
+    print("El contador es: ", contador)
+    contador -= 1 
+    
+# For con range
+
+for i in [0, 1, 2, 3, 4]:
+    print("El valor de i es: ", i)  
+
+
+
+
+
+
+
+
+
+
+
+
